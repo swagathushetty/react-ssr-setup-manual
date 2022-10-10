d51ca972bda5747d65f649043fd55ed8f14ddcc1
swagath shetty <swagath.shetty@gmail.com>
2022-10-10 18:14:28 +0530

    created a barebones SSR app setup

---
+++ src/index.js
@@ -0,0 +1,12 @@
+const express=require('express')
+const app=express()
+const React =require('react')
+const { render } = require('react-dom')
+const renderToString=require('react-dom/server').renderToString
+const Home =require('./client/components/Home').default
+app.get('/',(req,res)=>{
+ const content=renderToString(<Home />)
+ res.send(content)
+})
+
+app.listen(3000,()=>console.log('server started'))
