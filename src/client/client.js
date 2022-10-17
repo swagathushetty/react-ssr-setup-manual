c18e87bd46693eb41d36a2aa0803770393489db2
swagath shetty <swagath.shetty@gmail.com>
2022-10-17 13:25:06 +0530

    Added Client and Server side routing to the App

--- src/client/client.js
+++ src/client/client.js
@@ -1,7 +1,15 @@
 //startup point for client side application
 import React from 'react'
 import ReactDOM from 'react-dom'
-import Home from './components/Home'
+import {BrowserRouter} from 'react-router-dom'
 
-ReactDOM.hydrate(<Home />,document.querySelector('#root')) //render also works but shows warning
+import Routes from './Routes'
+
+
+//client side routing
+ReactDOM.hydrate(
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
+    ,document.querySelector('#root')) //render also works but shows warning
 
--- src/helpers/renderer..js
+++ src/helpers/renderer..js
@@ -1,9 +1,16 @@
 import React from 'react'
 import {renderToString} from 'react-dom/server'
-import Home from '../client/components/Home'
+import {StaticRouter} from 'react-router-dom'
+import Routes from '../client/Routes'
 
-export default ()=>{
-    const content=renderToString(<Home />)
+//server side routing
+//uses the same routes file as client side
+export default (req)=>{
+    const content=renderToString(
+        <StaticRouter location={req.url} context={{}}>
+            <Routes />
+        </StaticRouter>
+    )
 
 /*
  id root is added for the client side bundle react app to 
